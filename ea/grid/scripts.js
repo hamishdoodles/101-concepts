b7d3a61fe6d525d7e5afac0e6b2b8a223857d926
hamishdoodles@gmail.com <hamishdoodles@gmail.com>
2023-08-07 17:01:57 +0100

    Implement scroll matching on two grids

--- ea/grid/scripts.js
+++ ea/grid/scripts.js
@@ -1,37 +1,50 @@
-const wrapper = document.getElementById('wrapper');
-const sections = [...document.querySelectorAll(".section")];
-const closeButtons = [...document.querySelectorAll(".close-section")];
+const colors = ["#38a8cc", "#004444", "#884444", "#555588", "#a84466"];
 
-const openSection = (element) => {
-  wrapper.style.gridTemplateColumns = '1fr';
-  wrapper.style.height = '100vh';
-  sections.forEach(section => {
-    section.style.height = '100vh';
-  });
-  wrapper.classList.add('is-expanded');
-  wrapper.classList.add('has-expanded-item');
+const pagesContainer = document.getElementById('pages');
+const tilesContainer = document.getElementById('tiles');
 
-  // Scroll to the clicked element
-  element.scrollIntoView({ behavior: 'smooth' });
-};
+// Add 40 divs to both pages and tiles
+for (let i = 0; i < 40; i++) {
+  const color = colors[i % colors.length];
 
-const closeSection = () => {
-  wrapper.style.gridTemplateColumns = 'repeat(3, 1fr)';
-  wrapper.style.height = '300px';
-  sections.forEach(section => {
-    section.style.height = '300px';
-  });
-  wrapper.classList.remove('is-expanded');
-  wrapper.classList.remove('has-expanded-item');
-};
+  const pageDiv = document.createElement('div');
+  pageDiv.className = 'page';
+  pageDiv.style.backgroundColor = color;
+  pageDiv.innerHTML = `<div class="close-section">&times;</div>${i}`;
+  pagesContainer.appendChild(pageDiv);
 
-sections.forEach((element) => {
-  element.addEventListener("click", () => openSection(element));
+  const tileDiv = document.createElement('div');
+  tileDiv.className = 'tile';
+  tileDiv.style.backgroundColor = color;
+  tileDiv.textContent = i;
+  tilesContainer.appendChild(tileDiv);
+}
+
+// Measure total scrollable heights
+const pagesScrollHeight = pagesContainer.scrollHeight - pagesContainer.clientHeight;
+const tilesScrollHeight = tilesContainer.scrollHeight - tilesContainer.clientHeight;
+const scrollFactorPagesToTiles = tilesScrollHeight / pagesScrollHeight;
+const scrollFactorTilesToPages = pagesScrollHeight / tilesScrollHeight;
+
+let activeScrollContainer = null; // Keep track of the active scroll container
+
+// Sync scroll from pages to tiles
+pagesContainer.addEventListener('scroll', () => {
+  if (activeScrollContainer !== pagesContainer) return;
+  tilesContainer.scrollTop = pagesContainer.scrollTop * scrollFactorPagesToTiles;
 });
 
-closeButtons.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.stopPropagation();
-    closeSection();
-  });
+// Sync scroll from tiles to pages
+tilesContainer.addEventListener('scroll', () => {
+  if (activeScrollContainer !== tilesContainer) return;
+  pagesContainer.scrollTop = tilesContainer.scrollTop * scrollFactorTilesToPages;
+});
+
+// Determine the active scroll container on mouseover
+pagesContainer.addEventListener('mouseover', () => {
+  activeScrollContainer = pagesContainer;
+});
+
+tilesContainer.addEventListener('mouseover', () => {
+  activeScrollContainer = tilesContainer;
 });
